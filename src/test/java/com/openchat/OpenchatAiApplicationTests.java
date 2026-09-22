9ab979500ba5af8c7747f66bb1bbcf3729c47fd5
lakshmanasarathy <lakshmanasarathy7@gmail.com>
2026-09-22 16:21:25 +0530

    Tnitial commit

---
+++ src/test/java/com/openchat/OpenchatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
